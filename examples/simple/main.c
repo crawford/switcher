3d560e55f957fac7ad606fe8a329a4089d58c1e0
Alex Crawford <alex@acrawford.com>
2015-10-12 13:38:09 -0700

    examples: add simple

---
+++ examples/simple/main.c
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2015 Alex Crawford
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "switcher.h"
+
+#define HEADER_ADDR_A  0x0007FFF8
+#define HEADER_ADDR_B  0x000FFFF8
+#define HEADER_A       ((image_t *volatile)HEADER_ADDR_A)
+#define HEADER_B       ((image_t *volatile)HEADER_ADDR_B)
+
+void __attribute__ ((noreturn)) main(void)
+{
+	switcher_boot(switcher_choose(HEADER_A, HEADER_B));
+
+	/* Neither of the images are bootable! */
+	while (true)
+		;
+}
+
